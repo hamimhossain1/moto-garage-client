5383e3d294d80366703d8d7928c47b3e894f9dbf
HamimHossain <hamim.mgp@gmail.com>
2023-01-12 23:29:12 +0600

    jwt token done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -17,6 +17,28 @@ const Login = () => {
         const password = form.password.value;
         signIn(email, password)
             .then(result => {
+                const user = result.user;
+                console.log(user.email);
+
+                const currentUser = {
+                    email: user.email
+                }
+                console.log(currentUser);
+
+                //--- get jwt token ---//
+                fetch('http://localhost:5000/jwt', {
+                    method: 'POST',
+                    HEADERS: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(currentUser)
+                })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    localStorage.setItem('token', data.token);
+                })
+
                 toast.success('Login successfully...!!!')
                 // console.log(result.user);
                 navigate(from, { replace: true });
